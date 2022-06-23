c84af6ac7c5383efcdd6747468353dddc5400f2c
unknown <momen07996@gmail.com>
2022-06-23 04:29:45 -0700

    Sunnyside agency landing page

---
+++ Sunnyside agency landing page.js
@@ -0,0 +1,17 @@
+var click = function () {
+
+var click = document.getElementById("talkabouteggs2");
+
+click.style.display = "inline-block";
+}
+
+document.getElementById("orang").onclick = click;
+
+var click1 = function () {
+
+var click1 = document.getElementById("talkabouteggs3");
+
+click1.style.display = "inline-block";
+}
+
+document.getElementById("cherry").onclick = click1;
